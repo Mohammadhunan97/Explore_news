704111ec8ce5dd7fddae3ac3dfb26c1bebab06cf
mohammad <you@example.com>
2017-09-21 19:52:58 -0400

    removed api key

---
+++ app/components/feed.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text,View, Linking } from 'react-native';
+import styles from '../stylesheet';
+
+export default class Feed extends Component {
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+}
+
+
--- app/components/topics.js
+++ app/components/topics.js
@@ -3,16 +3,38 @@ import { Text,View, Linking } from 'react-native';
 import styles from '../stylesheet';
 
 export default class Topics extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      topicActive: false
+    }
+  }
+  getNews(){
+    this.setState({topicActive:true})
+  }
   render() {
+    topics = () => {
+      if(this.state.topicActive === false){
+        return(<View style={{marginTop:60}}>
+        <Text 
+        onPress={()=>this.getNews()}
+        style={styles.topic}>News</Text>
+        <Text 
+        style={styles.topic}>Tech</Text>
+        <Text 
+        style={styles.topic}>Sports</Text>
+        <Text 
+        style={styles.topic}>Entertainment</Text>
+        <Text 
+        style={styles.topic}>Finance</Text>
+        </View>)
+      }else{
+        return(<Text>News</Text>)
+      }
+    }
     return (
       <View style={styles.topics}>
-        <View style={{marginTop:60}}>
-        <Text style={styles.topic}>News</Text>
-        <Text style={styles.topic}>Tech</Text>
-        <Text style={styles.topic}>Sports</Text>
-        <Text style={styles.topic}>Entertainment</Text>
-        <Text style={styles.topic}>Finance</Text>
-        </View>
+      {topics()}
       </View>
     );
   }
